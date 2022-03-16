d6cd73c0909ba6013becf35269c17147130b29c7
Benjamin Pichon <benjamin@piloc.io>
2022-03-16 11:04:11 +0100

    Feature pause menu, background image

--- client.py
+++ client.py
@@ -1,19 +1,59 @@
 import pygame
 from Class.network import Network
 from Class.player import Player
+from Class.background import Background
 from dotenv import dotenv_values
 from functions import *
+
+pygame.init()
 
 # Load .env
 CONFIG = dotenv_values()
 
 # Create window
 WIN = pygame.display.set_mode((int(CONFIG.get('WINDOW_WIDTH')), int(CONFIG.get('WINDOW_HEIGHT'))))
+bg = Background('./sprite/background.png', [0,0])
 pygame.display.set_caption("Client")
 
+
+font = pygame.font.SysFont(None, 25)
+clock = pygame.time.Clock()
+
+def pause() :
+    paused = True
+
+    while paused is True:
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+            if event.type == pygame.KEYDOWN :
+                if event.key == pygame.K_ESCAPE:
+                    paused = False
+                elif event.key == pygame.K_q :
+                    pygame.quit()
+                    quit()
+        WIN.fill((255, 255, 255, 100))
+
+        message_to_screen("Jeu en pause", (0, 0, 0, 100))
+
+        pygame.display.update()
+        clock.tick(5)
+
+
+def text_objects(text, color) :
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
+def message_to_screen(msg, color):
+    textSurf, textRect = text_objects(msg, color)
+    textRect.center = (int(CONFIG.get('WINDOW_WIDTH'))/2), (int(CONFIG.get('WINDOW_HEIGHT'))/2)
+    WIN.blit(textSurf, textRect)
 # Redraw window with new values
 def redrawWindow(win, player, player2):
     win.fill((0, 0, 0))
+    win.blit(bg.image, bg.rect)
     player.draw(win)
     player2.draw(win)
     pygame.display.update()
@@ -21,8 +61,7 @@ def redrawWindow(win, player, player2):
 # Launch loop game
 def main():
     run = True
-    clock = pygame.time.Clock()
-    # Create network get data
+    # Current player
     n = Network()
     startPos = read_pos(n.getPos())
     startSide = n.getSide()
@@ -43,6 +82,7 @@ def main():
         p2.update()
 
         for event in pygame.event.get():
+                
             # Quit
             if event.type == pygame.QUIT:
                 run = False
@@ -50,12 +90,19 @@ def main():
 
             # Test
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE : 
+                    pause()
                 if event.key == pygame.K_a:
                     p.get_health(50)
                 if event.key == pygame.K_q:
                     p.get_damage(50)
+                # if event.key == pygame.K_UP : 
+                #     p.a_jump = True
+            
+
         
         p.move()
+        # p.jump()
         redrawWindow(WIN, p, p2)
         clock.tick(60)
 
